c2cb9bb8f497da6f147194bf23eda7e0329e763d
b00nw33 <pss.mrteo@gmail.com>
2024-11-03 19:16:42 +0800

    Added title and headers

--- main.py
+++ main.py
@@ -3,14 +3,18 @@
 import streamlit as st
 import pandas as pd
 
+st.title("My Awesome Streamlit App")
+
 col1, col2 = st.columns(2)
 
 with col1:
+    st.header("Column 1")
     name = st.text_input("Enter your name: ")
     st.button("Click me")
     st.link_button("Click **me** too!", url="/profile")
 
 with col2:
+    st.header("Column 2")
     if name:
         st.write(f"Hello, **{name}**, welcome!")
         st.metric("Your name", name)
