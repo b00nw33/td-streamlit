2ba40f6fa1a60aa28f22e8b95ddd2ef26cfe2b7f
b00nw33 <pss.mrteo@gmail.com>
2024-11-03 18:29:13 +0800

    Simple initial app

---
+++ main.py
@@ -0,0 +1,21 @@
+# Streamlit API Reference:
+# https://docs.streamlit.io/develop/api-reference
+import streamlit as st
+import pandas as pd
+
+col1, col2 = st.columns(2)
+
+with col1:
+    name = st.text_input("Enter your name: ")
+    st.button("Click me")
+    st.link_button("Click **me** too!", url="/profile")
+
+with col2:
+    if name:
+        st.write(f"Hello, **{name}**, welcome!")
+        st.metric("Your name", name)
+        st.metric("Number of characters in your name", len(name))
+
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+st.dataframe(df)
